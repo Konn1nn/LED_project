2fec22f572dedf465f2de1845150cd612c793aeb
Konn1nn <54936310+Konn1nn@users.noreply.github.com>
2021-09-01 14:59:35 +0000

    motor

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import explorerhat
 explorerhat.light.on()
 while True:
     if explorerhat.touch.one.is_pressed():
-        explorerhat.output.one.on()
-        time.sleep(5)
-        explorerhat.output.one.off()
+        for i in range(3):
+            explorerhat.output.one.on()
+            time.sleep(0.3)
+            explorerhat.output.one.off()
     else:
         explorerhat.output.one.off()
 
@@ -15,3 +16,9 @@ while True:
         explorerhat.light.blue.off()
     if explorerhat.touch.two.is_pressed():
         explorerhat.light.blue.on()
+    if explorerhat.touch.four.is_pressed():
+        explorerhat.motor.one.forward()
+        explorerhat.motor.two.backwards()
+        time.sleep(3)
+        explorerhat.motor.one.stop()
+        explorerhat.motor.two.stop()
