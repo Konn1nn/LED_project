d76f8ad792f6430388cba20b584e9806c2cee992
Konn1nn <54936310+Konn1nn@users.noreply.github.com>
2021-09-01 14:54:57 +0000

    LED work 5 sec

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import time
 import explorerhat
+
+
 explorerhat.light.on()
 while True:
     if explorerhat.touch.one.is_pressed():
-        explorerhat.output.on()
+        explorerhat.output.one.on()
+        time.sleep(5)
+        explorerhat.output.one.off()
     else:
-        explorerhat.output.off()
+        explorerhat.output.one.off()
 
     if explorerhat.touch.three.is_pressed():
         explorerhat.light.blue.off()
