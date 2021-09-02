ffaa06ec3c69920737b3aadbb149f45e74f0c886
Konn1nn <54936310+Konn1nn@users.noreply.github.com>
2021-09-02 21:10:46 +0000

    follow line first code

--- main_boi.py
+++ main_boi.py
@@ -100,8 +100,8 @@ def main():
             follow_line(red, green, blue)
             blink182(20, 0.1)
         elif explorerhat.touch.four.is_pressed():
-            explorerhat.motor.one.forwards(-10)
-            explorerhat.motor.two.backwards()
+            explorerhat.motor.one.forwards(1)
+            explorerhat.motor.two.backwards(50)
             blink182(1, 1)
             explorerhat.motor.one.stop()
             explorerhat.motor.two.stop()
