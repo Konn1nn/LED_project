4e058d4fa98ae94aee0476cdf80c3405a758d808
hakonhakonarson <konn1nn1@gmail.com>
2021-09-08 13:35:05 +0000

    changed which motor goes first in follow_line()

--- main_boi.py
+++ main_boi.py
@@ -104,7 +104,9 @@ def follow_line(r_set, g_set, b_set):
         if on_line(r_set, g_set, b_set):
             explorerhat.motor.two.forwards(50)
             explorerhat.motor.one.forwards(50)
-            time.sleep(0.3)
+            time.sleep(0.1)
+            explorerhat.motor.two.stop()
+            explorerhat.motor.one.stop()
         else:
             explorerhat.motor.one.stop()
             explorerhat.motor.two.stop()
