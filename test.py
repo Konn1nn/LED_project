270b5b0b7c5b201de57cc11b37affdafd3a1c393
hakonhakonarson <konn1nn1@gmail.com>
2021-09-15 11:05:26 +0000

    Created test code to read from the ultrasonic sensor

--- test.py
+++ test.py
@@ -5,9 +5,12 @@ if __name__ == "__main__":
     keep_running = True
     explorerhat.light.on()
     distance_sensor = explorerhat.AnalogInput(1)
+    trigger = explorerhat.Output(8)
 
     while keep_running:
+        trigger.on()
         distance = distance_sensor.read()
+        trigger.off()
         print(distance)
         if explorerhat.touch.four.is_pressed():
             keep_running = False
