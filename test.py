ba60ca2fdd3050c7761b607e5426483cb6361110
hakonhakonarson <konn1nn1@gmail.com>
2021-09-15 10:41:57 +0000

    Created test code to read from the ultrasonic sensor

--- test.py
+++ test.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     explorerhat.light.on()
 
     while keep_running:
-        some_var = explorerhat.Analog.one.read()
+        some_var = explorerhat.read(1)
         if explorerhat.touch.four.is_pressed():
             keep_running = False
