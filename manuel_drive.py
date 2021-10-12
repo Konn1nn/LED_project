fc485f4ab32b1ae9e4fc597e8dbab908c116cc3f
hakonhakonarson <konn1nn1@gmail.com>
2021-10-12 13:10:13 +0000

    trying manual drive

--- manuel_drive.py
+++ manuel_drive.py
@@ -1,4 +1,3 @@
-import getch
 import explorerhat
 import time
 
@@ -22,6 +21,21 @@ def stop():
     explorerhat.motor.two.stop()
 
 
+def getch():
+  import sys, tty, termios
+  old_settings = termios.tcgetattr(0)
+  new_settings = old_settings[:]
+  new_settings[3] &= ~termios.ICANON
+  try:
+    termios.tcsetattr(0, termios.TCSANOW, new_settings)
+    ch = sys.stdin.read(1)
+  finally:
+    termios.tcsetattr(0, termios.TCSANOW, old_settings)
+  return ch
+
+
+
+
 
 
 if __name__ == "__main__":
@@ -33,10 +47,11 @@ if __name__ == "__main__":
     time.sleep(0.2)
     explorerhat.light.four.on()
 
+    print("\nchar is '" + getch() + "'\n")
 
     kb_input = ''
     while kb_input != 'q':
-        kb_input = getch.getch()
+        kb_input = getch()
         if kb_input == 'w':
             drive()
         elif kb_input == 's':
