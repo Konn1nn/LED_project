c6cd2b80881dfaa3c56722bb1fb88f5fd7cd9085
hakonhakonarson <konn1nn1@gmail.com>
2021-10-12 13:49:45 +0000

    trying manual drive

--- manuel_drive.py
+++ manuel_drive.py
@@ -10,7 +10,7 @@ def drive(forward = True):
         explorerhat.motor.one.forwards(motor_one)
         explorerhat.motor.two.forwards(motor_two)
         time.sleep(0.05)
-        explorerhat.motor.one.forwards(25)
+        explorerhat.motor.one.forwards(30)
         explorerhat.motor.two.forwards(25)
     else:
         explorerhat.motor.one.backwards(motor_one)
