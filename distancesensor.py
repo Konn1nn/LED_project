5dce528073b97e82ba7c27356e8bd25dc3bafc11
hakonhakonarson <konn1nn1@gmail.com>
2021-10-20 14:06:14 +0000

    Testing distance

--- distancesensor.py
+++ distancesensor.py
@@ -4,6 +4,8 @@ import time
 class DistanceSensor:
     def __init__(self):
         setup()
+        self.trigger = 12
+        self.echo = 22
 
 
     def setup(self):
@@ -14,28 +16,31 @@ class DistanceSensor:
         GPIO_TRIGGER = 12 # output pin 2 on explorerhat
         GPIO_ECHO = 22 # input pin 2 on eplorerhat
 
+        self.trigger = 12
+        self.echo = 22
+
         # set GPIO direction (IN / OUT)
-        GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-        GPIO.setup(GPIO_ECHO, GPIO.IN)
+        GPIO.setup(self.trigger, GPIO.OUT)
+        GPIO.setup(self.echo, GPIO.IN)
 
 
     def distance(self):
         # set Trigger to HIGH
-        GPIO.output(GPIO_TRIGGER, True)
+        GPIO.output(self.trigger, True)
 
         # set Trigger after 0.01ms to LOW
         time.sleep(0.00001)
-        GPIO.output(GPIO_TRIGGER, False)
+        GPIO.output(self.trigger, False)
 
         StartTime = time.time()
         StopTime = time.time()
 
         # save StartTime
-        while GPIO.input(GPIO_ECHO) == 0:
+        while GPIO.input(self.echo) == 0:
             StartTime = time.time()
 
         # save time of arrival
-        while GPIO.input(GPIO_ECHO) == 1:
+        while GPIO.input(self.echo) == 1:
             StopTime = time.time()
 
         # time difference between start and arrival
@@ -49,8 +54,9 @@ class DistanceSensor:
 
 if __name__ == '__main__':
     try:
+        sensor = DistanceSensor()
         while True:
-            dist = distance()
+            dist = sensor.distance()
             print("Measured Distance = %.1f cm" % dist)
             time.sleep(1)
 
