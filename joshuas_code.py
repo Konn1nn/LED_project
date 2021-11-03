fa6b03d5243a0cd992d611185047a0e22737ced5
hakonhakonarson <konn1nn1@gmail.com>
2021-11-03 10:32:15 +0000

    added blinking light when stopped

--- joshuas_code.py
+++ joshuas_code.py
@@ -2,7 +2,6 @@
 
 import sys
 import signal
-sys.path.append('/home/pi/.local/lib/python3.7/site-packages/simple_pid')
 import explorerhat
 import time
 import board
@@ -233,6 +232,7 @@ def main():
         time.sleep(0.01)
         distance = ds.distance()
         while(distance < 10):
+            explorerhat.output.two.blink(0.1,0.1)
             set_throttles(0, 0)
             print("There is a big thing %.1f cm in front of me" % distance)
             time.sleep(0.02)
@@ -240,7 +240,7 @@ def main():
             if (time.time() - time_since) > 3:
                 play_sound()
                 time_since = time.time()
-
+        explorerhat.output.two.off()
 
 if __name__ == "__main__":
     main()
