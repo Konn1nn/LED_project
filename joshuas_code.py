3080f6f320295af744b6d6092a8b7f4dfee1db2a
hakonhakonarson <konn1nn1@gmail.com>
2021-11-09 23:26:15 +0000

    Added new sounds

--- joshuas_code.py
+++ joshuas_code.py
@@ -185,7 +185,7 @@ def main():
     startup()
 
     # instantiate a PID controller with kp=20, ki=4, kd=2.5, and setpoint=0
-    pid_controller = simple_pid.PID(20, 4, 2.5, setpoint=0)
+    pid_controller = simple_pid.PID(20, 40, 2.5, setpoint=0)
 
     base_throttle = 40
     offset = 1  # the motors do not have equal power, so we offset the throttle slightly to drive in a straight line
